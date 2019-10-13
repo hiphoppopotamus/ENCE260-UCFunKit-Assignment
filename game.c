015aded79fe56b667e71d1f179389487f72e6a75
Euan Widjaja <ewi45@uclive.ac.nz>
2019-10-13 15:14:45 +1300

    Updated game.c and modularised code. Game now runs sideways and with an updated font.

--- game.c
+++ game.c
@@ -3,145 +3,204 @@
 #include "ledmat.h"
 #include "tinygl.h"
 #include "../fonts/font5x7_1.h"
-#include "../fonts/fontPSR.h"
+#include "fonts/fontPSR.h"
+#include "fonts/fontPSRSideways.h"
 #include "navswitch.h"
 #include "ir_uart.h"
+#include <avr/io.h>
+#include <stdbool.h>
+#include "button.h"
+#include "led.h"
 
-#define ROCK_LOGO "*"
-
-/* Define polling rate in Hz.  */
 #define LOOP_RATE 300
-
-/* Define text update rate in Hz.  */
-#define MESSAGE_RATE 8
+#define MESSAGE_RATE 20
 #define PACER_RATE 500
 
+#define ROCK '@'
+#define PAPER '#'
+#define SCISSORS '$'
 
-void display_title (void)
+typedef enum {
+    STATE_TITLE,
+    STATE_START
+} state_t;
+
+void initialise (void)
 {
-    tinygl_text_mode_set(TINYGL_TEXT_MODE_SCROLL);
-    /*
-        tinygl_font_set(&font5x7_1);
-        tinygl_text("ROCK ");
-        tinygl_font_set(&fontPSR);
-        tinygl_text("R ");
-        tinygl_font_set(&font5x7_1);
-        tinygl_text("PAPER ");
-        tinygl_font_set(&fontPSR);
-        tinygl_text("P ");
-        tinygl_font_set(&font5x7_1);
-        tinygl_text("SCISSORS ");
-        tinygl_font_set(&fontPSR);
-        tinygl_text("S ");
-    */
-    tinygl_font_set(&fontPSR);
-    tinygl_text_speed_set(MESSAGE_RATE);
-    tinygl_text("RPS");
+    system_init ();
+    led_init ();
+    button_init ();
+    navswitch_init();
+    ir_uart_init ();
+    tinygl_init(PACER_RATE);
+    pacer_init (PACER_RATE);
+    led_set (LED1, 0);
 }
 
-int set_start (int start)
+void io_update (void)
 {
-    if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
-        if (start == 0) {
-            //tinygl_font_set(&font5x7_1); // basic abcd font
-            tinygl_text_mode_set(TINYGL_TEXT_MODE_STEP);
-            start = 1;
+    pacer_wait ();
+    tinygl_update ();
+    navswitch_update();
+    button_update();
+
+}
+
+int winner(char outgoingCharacter, char incomingCharacter)
+{
+    int result = 0;
+    if (outgoingCharacter == ROCK) {
+        if (incomingCharacter == ROCK) {
+            result = 0; // Draw
+        } else if (incomingCharacter == PAPER) {
+            result = 2; //player 2 (other) wins
+        } else if (incomingCharacter == SCISSORS) {
+            result = 1;
         }
-        /*
-                else if (start == 1) {
-                    display_title();
-                    start = 0;
-                }
-        */
+    } else if (outgoingCharacter == PAPER) {
+        if (incomingCharacter == PAPER) {
+            result = 0;
+        } else if (incomingCharacter == SCISSORS) {
+            result = 2; //player 2 (other) wins
+        } else if (incomingCharacter == ROCK) {
+            result = 1;
+        }
+    } else if (outgoingCharacter == SCISSORS) {
+        if (incomingCharacter == SCISSORS) {
+            result = 0;
+        } else if (incomingCharacter == ROCK) {
+            result = 2; //player 2 (other) wins
+        } else if (incomingCharacter == PAPER) {
+            result = 1;
+        }
     }
-    return start;
+    return result;
 }
 
+char set_character (char character, bool sent)
+{
+    if (navswitch_push_event_p(NAVSWITCH_NORTH) && sent == false) {
+        if (character == ROCK) {
+            character = SCISSORS;
+        } else if (character == SCISSORS) {
+            character = PAPER;
+        } else if (character == PAPER) {
+            character = ROCK;
+        }
+    }
+
+    if (navswitch_push_event_p(NAVSWITCH_SOUTH) && sent == false) {
+        if (character == ROCK) {
+            character = PAPER;
+        } else if (character == PAPER) {
+            character = SCISSORS;
+        } else if (character == SCISSORS) {
+            character = ROCK;
+        }
+    }
+    return character;
+}
+
+void start_game (void)
+{
+
+    char score = '0';
+    //int otherScore = 0;
+
+    bool sent = false;
+    bool received = false;
+
+    char character = ROCK;
+    char incomingCharacter = ROCK;
+
+    while (1) {
+        io_update();
+        tinygl_text_mode_set(TINYGL_TEXT_MODE_STEP);
+        character = set_character(character, sent);
+        display_character (character);
+
+        if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
+            ir_uart_putc(character);
+            sent = true;
+        }
+
+        if (ir_uart_read_ready_p()) {
+            char char_buff = ir_uart_getc();
+            if (char_buff == ROCK || char_buff == PAPER || char_buff == SCISSORS) {
+                incomingCharacter = char_buff;
+                received = true;
+                led_set (LED1, 1);
+            }
+        }
+
+        if (received == true && sent == true) {
+            led_set (LED1, 0);
+            int won = winner(character, incomingCharacter);
+            if (won == 1) {
+                score++;
+            }
+            display_score(score);
+            received = false;
+            sent = false;
+        }
+    }
+}
+
+
+//FROM LAB FOR this function was written by some shit or modified by me to do shit
 void display_character (char character)
 {
     char buffer[2];
-
     buffer[0] = character;
     buffer[1] = '\0';
     tinygl_text (buffer);
 }
 
+void display_score (int score)
+{
+    while (1) {
+        io_update();
+        display_character(score);
+        if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
+            break;
+        }
+    }
+}
+
+void display_title (void)
+{
+    tinygl_text_mode_set(TINYGL_TEXT_MODE_SCROLL);
+    tinygl_text_dir_set (TINYGL_TEXT_DIR_ROTATE);
+    tinygl_font_set(&fontPSRSideways);
+    tinygl_text_speed_set(MESSAGE_RATE);
+    tinygl_text("*ROCK@*PAPER#*SCISSORS$*");
+    while (1) {
+        //ledmat_init();
+        io_update();
+        if (button_push_event_p (0)) {
+            tinygl_clear();
+            break;
+        }
+    }
+}
+
 int main (void)
 {
-    system_init();
-    tinygl_init(PACER_RATE);     // Loop Rate is the same as pacer rate
-    display_title();             // Displays title screen
-    navswitch_init();
-
-    /*
-        char sent_char = 'A';
-        char received_char = 'A';
-        int sent = 0;
-        int received = 0;
-    */
-
-    char character = 'R';
-    //char incomingCharacter = 'R';
-    int start = 0;
-
-    pacer_init (PACER_RATE);
-
+    initialise();
+    state_t state = STATE_TITLE;
     while (1) {
-        pacer_wait ();
-        tinygl_update ();
-        navswitch_update();
-
-        start = set_start(start);            // Sets starts game or back to title screen
-
-        if (start == 1) {
-            tinygl_text_mode_set(TINYGL_TEXT_MODE_STEP);
-
-/*
-            if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
-                ir_uart_putc(character);
-                //received = 0;
-            }
-*/
-
-            /* Character received event */
-
-/*
-            if (ir_uart_read_ready_p()) {
-                char char_buff = ir_uart_getc();
-                if (char_buff >= 33 && char_buff <= 126) {
-                    //received_char = char_buff;
-                    //received = 1;
-                }
-            }
-*/
-
-
-            /* Increment character if NORTH is pressed.  */
-
-            if (navswitch_push_event_p(NAVSWITCH_NORTH)) {
-                if (character == 'R') {
-                    character = 'S';
-                } else if (character == 'S') {
-                    character = 'P';
-                } else if (character == 'P') {
-                    character = 'R';
-                }
-            }
-
-
-            /* Decrement character if SOUTH is pressed.  */
-
-            if (navswitch_push_event_p(NAVSWITCH_SOUTH)) {
-                if (character == 'R') {
-                    character = 'P';
-                } else if (character == 'P') {
-                    character = 'S';
-                } else if (character == 'S') {
-                    character = 'R';
-                }
-            }
-
-            display_character (character);
+        io_update();
+        switch (state) {
+        case STATE_TITLE:
+            display_title();
+            state = STATE_START;
+            break;
+        case STATE_START:
+            start_game();
+            state= STATE_TITLE;
+            break;
+        default:
+            break;
         }
     }
 }
