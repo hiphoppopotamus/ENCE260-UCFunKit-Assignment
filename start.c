625e653f15641a84791517c19fadc9335621f9ae
ewi45 <ewi45@uclive.ac.nz>
2019-10-16 18:08:18 +1300

    modularised start module which includes the start function

---
+++ start.c
@@ -0,0 +1,59 @@
+#include "start.h"
+
+/**
+ * Key function that starts game.
+ */
+void start_game (void)
+{
+    char score = '0';
+    char opponent_score = '0';
+
+    int round = 1;
+    int won = 0;
+
+    bool sent = false;
+    bool received = false;
+
+    char character = ROCK;
+    char incomingCharacter = ROCK;
+
+    while (score != '6' && opponent_score != '6' && round < 10) {
+        io_update ();
+        display_round (round);
+        tinygl_text_mode_set(TINYGL_TEXT_MODE_STEP);
+        while (1) {
+            io_update ();
+            character = set_character(character, sent);
+            display_character (character);
+            display_score_pixel (score);
+            if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
+                ir_uart_putc(character);
+                sent = true;
+            }
+            if (ir_uart_read_ready_p()) {
+                char char_buff = ir_uart_getc();
+                if (char_buff == ROCK || char_buff == PAPER || char_buff == SCISSORS) {
+                    incomingCharacter = char_buff;
+                    received = true;
+                    led_set (LED1, 1);
+                }
+            }
+            if (received == true && sent == true) {
+                tinygl_clear();
+                led_set (LED1, 0);
+                won = set_winner(character, incomingCharacter);
+                if (won == 1) {
+                    score++;
+                } else if (won == 2) {
+                    opponent_score++;
+                }
+                display_score(score);
+                received = false;
+                sent = false;
+                round++;
+                break;
+            }
+        }
+    }
+    display_results_screen (score, opponent_score);
+}
