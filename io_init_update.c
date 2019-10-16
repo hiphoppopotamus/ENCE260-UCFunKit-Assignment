31a74f16f89bf69387c194ed985566153929aec3
ewi45 <ewi45@uclive.ac.nz>
2019-10-16 15:17:43 +1300

    io_init_update class for init and updates

---
+++ io_init_update.c
@@ -0,0 +1,33 @@
+#include "io_init_update.h"
+
+/**
+ * Initialises the fun kit's key functionalities:
+ * system, button, navswitch, ir_uart, ledmat, tinygl, pacer, and led.
+ * led_set (LED1, 0) is called to turn led off,
+ * as initialising led turns it on by default.
+ */
+void io_initialise (void)
+{
+    system_init ();
+    button_init ();
+    display_init ();
+    navswitch_init ();
+    ir_uart_init ();
+    tinygl_init (PACER_RATE);
+    pacer_init (PACER_RATE);
+    led_init ();
+    led_set (LED1, 0);
+    ledmat_init ();
+}
+
+/**
+ * A method that updates tinygl, navswitch, button,
+ * and calls pacer_wait().
+ */
+void io_update (void)
+{
+    pacer_wait ();
+    tinygl_update ();
+    navswitch_update ();
+    button_update ();
+}
