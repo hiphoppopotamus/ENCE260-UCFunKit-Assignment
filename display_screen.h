1c33ed16eda3bad2e6312799a9352fea5d3b5af5
ewi45 <ewi45@uclive.ac.nz>
2019-10-17 14:11:54 +1300

    Edited header comments

--- display_screen.h
+++ display_screen.h
@@ -1,3 +1,14 @@
+/** GROUP 307 ewi45 cjm356
+    @file   display_screen.c
+    @author Euan Widaja, Cam Maslin
+    @date   16 October 2019
+    @brief  Displays the desired images/text on the LED matrix screen.
+
+    @note   Contains all functions associated with displaying
+            required by the game.
+*/
+
+
 #ifndef DISPLAY_TITLE_H
 #define DISPLAY_TITLE_H
 
@@ -13,6 +24,7 @@
 
 #include "fontPSRSideways.h"
 
+
 /**
  * Displays the game's title screen
  * using the tinygl module and a predefined font
@@ -21,34 +33,51 @@
  */
 void display_title (void);
 
+
 /**
  * Takes a char character, creates a char array,
  * and inserts the character in.
- * Then displays the string with tinygl_text.
+ * Then displays the string by passing it
+ * as a parameter of tinygl_text.
  *
  * Function derived from 'lab3-ex2.c',
  * written by the ENCE260 team.
+ *
+ * @param character char to be displayed
  */
 void display_character (char character);
 
+
 /**
  * Displays the current score that current player has.
+ * Calls passes its parameter score to display_character ()
+ * for score displaying.
+ *
+ * @param score int to be displayed
  */
 void display_score (int score);
 
+
 /**
  * Displays a score bar utilising the ledmat module.
- * Takes a char score as a parameter to indicate
- * the score bar.
+ * Takes a char score as a parameter to indicate a
+ * score bar using tinygl_point().
+ *
+ * @param score char to indicate pixel coordinates
  */
 void display_score_pixel (char score);
 
+
 /**
  * Displays each round of the game, with a maximum of 9 rounds.
- * Takes an int round as a parameter, and is called each time
- * before the start of a round.
+ * Takes an int round as a parameter and concatenates it with
+ * the string "ROUND-". Resulting string then passed as a parameter of
+ * tinygl_text. Called each time before the start of a round.
+ *
+ * @param round int indicates round of the game
  */
 void display_round (int round);
+
 
 /**
  * Takes an int score that indicates the player's score,
@@ -58,6 +87,9 @@ void display_round (int round);
  * If player's score is greater than opponent's, then player wins.
  * If opponent's score is greater than player's, opponent wins.
  * If the player's and the opponent's scores are equal, nobody wins.
+ *
+ * @param score int player's score
+ * @param opponent_score int opponent's score
  */
 void display_results_screen (int score, int opponent_score);
 
