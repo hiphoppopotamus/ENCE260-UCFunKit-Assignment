4a45ce0d475fa18c1617dd98b4e0cbd59e53fcb9
ewi45 <ewi45@uclive.ac.nz>
2019-10-17 14:13:19 +1300

    Edited comments

--- set.c
+++ set.c
@@ -2,8 +2,11 @@
     @file   set.c
     @author Euan Widaja, Cam Maslin
     @date   16 October 2019
-    @brief  Functions to change the user options during each round and includes
-    a function to decide the winner of each round.
+    @brief  Set user options of characters to display
+            and winners for each round
+
+    @note   Includes functions to change characters between
+            ROCK, PAPER, SCISSORS and to set winner of each round
 */
 
 
@@ -14,10 +17,14 @@
  * Sets a char ROCK, PAPER or SCISSORS; its symbols defined above,
  * when navswitch is pushed north or south.
  * Returns the char to be set.
+ *
+ * @param character char to be set
+ * @param sent boolean indicating if character has been sent
+ * @return character to be displayed
  */
 char set_character (char character, bool sent)
 {
-    if (navswitch_push_event_p(NAVSWITCH_NORTH) && sent == false) {
+    if (navswitch_push_event_p (NAVSWITCH_NORTH) && sent == false) {
         if (character == ROCK) {
             character = SCISSORS;
         } else if (character == SCISSORS) {
@@ -27,7 +34,7 @@ char set_character (char character, bool sent)
         }
     }
 
-    if (navswitch_push_event_p(NAVSWITCH_SOUTH) && sent == false) {
+    if (navswitch_push_event_p (NAVSWITCH_SOUTH) && sent == false) {
         if (character == ROCK) {
             character = PAPER;
         } else if (character == PAPER) {
@@ -44,32 +51,36 @@ char set_character (char character, bool sent)
  * Called to set winner of game.
  * Returns an int result that determines the winner of the round.
  * 0 if its a draw, 1 if you win and 2 if the other player wins.
+ *
+ * @param player_character char of player
+ * @param opponent_character char of opponent
+ * @return result int indicates winner of game
  */
-int set_winner(char outgoingCharacter, char incomingCharacter)
+int set_winner(char player_character, char opponent_character)
 {
     int result = 0;
-    if (outgoingCharacter == ROCK) {
-        if (incomingCharacter == ROCK) {
+    if (player_character == ROCK) {
+        if (opponent_character == ROCK) {
             result = 0; // Draw
-        } else if (incomingCharacter == PAPER) {
-            result = 2; //player 2 (other) wins
-        } else if (incomingCharacter == SCISSORS) {
+        } else if (opponent_character == PAPER) {
+            result = 2; // Opponent wins
+        } else if (opponent_character == SCISSORS) {
+            result = 1; // Player wins
+        }
+    } else if (player_character == PAPER) {
+        if (opponent_character == PAPER) {
+            result = 0;
+        } else if (opponent_character == SCISSORS) {
+            result = 2;
+        } else if (opponent_character == ROCK) {
             result = 1;
         }
-    } else if (outgoingCharacter == PAPER) {
-        if (incomingCharacter == PAPER) {
+    } else if (player_character == SCISSORS) {
+        if (opponent_character == SCISSORS) {
             result = 0;
-        } else if (incomingCharacter == SCISSORS) {
-            result = 2; //player 2 (other) wins
-        } else if (incomingCharacter == ROCK) {
-            result = 1;
-        }
-    } else if (outgoingCharacter == SCISSORS) {
-        if (incomingCharacter == SCISSORS) {
-            result = 0;
-        } else if (incomingCharacter == ROCK) {
-            result = 2; //player 2 (other) wins
-        } else if (incomingCharacter == PAPER) {
+        } else if (opponent_character == ROCK) {
+            result = 2;
+        } else if (opponent_character == PAPER) {
             result = 1;
         }
     }
