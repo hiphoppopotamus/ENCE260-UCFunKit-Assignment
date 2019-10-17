dec3c9e2978da95cb790283f889c97f43e0b5b44
ewi45 <ewi45@uclive.ac.nz>
2019-10-17 14:13:08 +1300

    Edited comments

--- io_init_update.c
+++ io_init_update.c
@@ -3,6 +3,8 @@
     @author Euan Widaja, Cam Maslin
     @date   16 October 2019
     @brief  Initialises and updates all inputs and outputs for the program.
+
+    @note   Contains functions related to io initialisation and updating.
 */
 
 
@@ -24,15 +26,15 @@ void io_initialise (void)
     ir_uart_init ();
     tinygl_init (PACER_RATE);
     pacer_init (PACER_RATE);
-    led_init ();
-    led_set (LED1, 0);
+    led_init (); // Initialises LED and turns LED on
+    led_set (LED1, 0); // Turns LED off after initialisation
     ledmat_init ();
 }
 
 
 /**
  * A method that updates tinygl, navswitch, button,
- * and calls pacer_wait().
+ * and calls pacer_wait ().
  */
 void io_update (void)
 {
