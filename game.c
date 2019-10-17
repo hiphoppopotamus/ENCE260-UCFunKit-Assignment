cb078c891525c625e42f9e050a9024531f380827
ewi45 <ewi45@uclive.ac.nz>
2019-10-17 14:13:00 +1300

    Edited comments

--- game.c
+++ game.c
@@ -1,9 +1,9 @@
 /** GROUP 307 ewi45 cjm356
-    @file   game.c
-    @author Euan Widaja, Cam Maslin
-    @date   9 October 2019
-    @brief  A simple rock, paper, scissors game with a maximum of
-            9 rounds and a score board.
+    @file     game.c
+    @author   Euan Widaja, Cam Maslin
+    @date     9 October 2019
+    @brief    A simple rock, paper, scissors game with a maximum of
+              9 rounds and a score board.
 
     @defgroup game A simple rock, paper, scissors game .
 */
@@ -15,7 +15,7 @@
 #include "start.h"
 
 
-/**
+/*
  * Enum that defines a state in which the game is in.
  * STATE_TITLE: Title Screen
  * STATE_START: Start Screen
@@ -31,17 +31,17 @@ typedef enum {
  */
 int main (void)
 {
-    io_initialise();
+    io_initialise ();
     state_t state = STATE_TITLE;
     while (1) {
-        io_update();
+        io_update ();
         switch (state) {
         case STATE_TITLE:
-            display_title();
+            display_title ();
             state = STATE_START;
             break;
         case STATE_START:
-            start_game();
+            start_game ();
             state= STATE_TITLE;
             break;
         default:
