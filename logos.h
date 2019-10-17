32fe9f025ff41a771d1ca4a440e0e0b253b7a114
ewi45 <ewi45@uclive.ac.nz>
2019-10-17 14:18:38 +1300

    Added module comment

--- logos.h
+++ logos.h
@@ -1,8 +1,18 @@
+/** GROUP 307 ewi45 cjm356
+    @file   logos.h
+    @author Euan Widaja, Cam Maslin
+    @date   16 October 2019
+    @brief  Defines macro variables for rock '@', paper '#', scissors '$'
+            to simpplify usage of symbols in code.
+*/
+
+
 #ifndef LOGO_H
 #define LOGO_H
 
 #define ROCK '@'
 #define PAPER '#'
 #define SCISSORS '$'
+
 
 #endif
